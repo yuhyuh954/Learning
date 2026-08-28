220dce40597ae05e545639d815865ae99686431a
John Elbert <Johnelbert709@gmail.com>
2026-08-27 20:31:03 -0400

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,13 @@
+name = 'Alice' #variable declaring the name
+print(name)     #prints name type
+print(type(name))    #prints type of varibale e.g string
+
+is_student = True  #variable declaring if the person is a student or not
+print(is_student, type(is_student))
+
+age = 20
+print(age, type(age))
+
+score = 80.5
+print(isinstance(score, float))
+print(score, type(score))
